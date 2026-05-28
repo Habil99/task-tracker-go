301417b8c77f786c09f2ce07e22f54b94125b5d5
Habil99 <abiyevhabil3@gmail.com>
2026-05-28 16:13:54 +0400

    refactor: move command handlers for task management operations to handlers.go

---
+++ handlers.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"log"
+	"strconv"
+)
+
+func handleList(args []string) {
+	s, err := ParseStatus(args[2])
+
+	if err != nil {
+		log.Fatalf("%v. use: %s,%s,%s", err, Todo, InProgress, Done)
+	}
+
+	tasks := getTasksByStatus(s)
+	log.Printf("%+v", tasks)
+}
+
+func handleAdd(args []string) {
+	addTask(TaskDescription(args[2]))
+}
+
+func handleDelete(args []string) {
+	id, err := strconv.Atoi(args[2])
+
+	if err != nil {
+		log.Fatal("Please provide correct id format (int)")
+	}
+
+	deleteTask(TaskID(id))
+}
+
+func handleMarkInProgress(args []string) {
+	id, err := strconv.Atoi(args[2])
+
+	if err != nil {
+		log.Fatal("Please provide correct id format (int)")
+	}
+
+	updateTask(
+		TaskID(id),
+		UpdateDto{status: InProgress.Pointer()},
+	)
+}
+
+func handleMarkDone(args []string) {
+	id, err := strconv.Atoi(args[2])
+
+	if err != nil {
+		log.Fatal("Please provide correct id format (int)")
+	}
+
+	updateTask(
+		TaskID(id),
+		UpdateDto{status: Done.Pointer()},
+	)
+}
+
+func handleUpdate(args []string) {
+	id, err := strconv.Atoi(args[2])
+
+	if err != nil {
+		log.Fatal("Please provide correct id format (int)")
+	}
+
+	if args[3] != "" {
+		updateTask(
+			TaskID(id),
+			UpdateDto{
+				description: TaskDescription(args[3]).TaskDescriptionPointer(),
+			})
+	} else {
+		log.Fatal("Please provide description")
+	}
+}
--- main.go
+++ main.go
@@ -3,84 +3,29 @@ package main
 import (
 	"log"
 	"os"
-	"strconv"
 )
 
 func main() {
 	args := os.Args
 
-	if len(args) == 2 && args[1] == List.String() {
-		tasks := getTasks()
-		log.Printf("%+v", tasks)
+	if len(args) < 2 {
 		return
 	}
 
-	if len(args) == 3 {
-		if args[1] == List.String() {
-			s, err := ParseStatus(args[2])
-
-			if err != nil {
-				log.Fatalf("%v. use: %s,%s,%s", err, Todo, InProgress, Done)
-			}
-
-			tasks := getTasksByStatus(s)
-			log.Printf("%+v", tasks)
-
-			return
-		}
-
-		if args[1] == Add.String() && args[2] != "" {
-			addTask(TaskDescription(args[2]))
-
-			return
-		}
-
-		if args[2] != "" {
-			id, err := strconv.Atoi(args[2])
-
-			if err != nil {
-				log.Fatal("Please provide correct id format (int)")
-			}
-
-			if args[1] == Delete.String() {
-				deleteTask(TaskID(id))
-			}
-
-			if args[1] == MarkInProgress.String() {
-				updateTask(
-					TaskID(id),
-					UpdateDto{status: InProgress.Pointer()},
-				)
-			}
-
-			if args[1] == MarkDone.String() {
-				updateTask(
-					TaskID(id),
-					UpdateDto{status: Done.Pointer()},
-				)
-			}
-
-		}
-
-		return
-	}
-
-	if len(args) == 4 && args[1] == Update.String() {
-		id, err := strconv.Atoi(args[2])
-
-		if err != nil {
-			log.Fatal("Please provide correct id format (int)")
-		}
-
-		if args[3] != "" {
-			updateTask(
-				TaskID(id),
-				UpdateDto{
-					description: TaskDescription(args[3]).TaskDescriptionPointer(),
-				})
-		} else {
-			log.Fatal("Please provide description")
-		}
-
+	switch args[1] {
+	case List.String():
+		handleList(args)
+	case Add.String():
+		handleAdd(args)
+	case Delete.String():
+		handleDelete(args)
+	case Update.String():
+		handleUpdate(args)
+	case MarkInProgress.String():
+		handleMarkInProgress(args)
+	case MarkDone.String():
+		handleMarkDone(args)
+	default:
+		log.Fatalf("unknown command: %s", args[1])
 	}
 }
